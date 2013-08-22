74700611ccb70bb652be740a034a641c95d3daf2
Fábio Pedrosa <1111156@isep.ipp.pt>
2013-08-22 19:02:02 +0100

    Add time hh:mm:ss to notifications

--- background.js
+++ background.js
@@ -1,32 +1,41 @@
+function hhmmss(sec) {
+    var h = Math.floor(sec / 60 / 60);
+    sec = sec - h * 60 * 60;
+    var m = Math.floor(sec / 60);
+    sec = sec - m * 60;
+    var s = Math.floor(sec);
+
+    return h + ":" + m + ":" + s;
+}
+
 function getMBdisponiveis(){
     try {
         var req = new XMLHttpRequest();
         var url = "http://redirect.kanguru.pt/kanguru/RedirectAlertaConsumo.aspx?final_url=aHR0cDovL3JlZGlyZWN0Lmthbmd1cnUucHQvS2FuZ3VydS9SZWRpcmVjdENDU2VtU2FsZG8uYXNweA%3d%3d&msisdn=0BdTiF50t7hnu8l4Mlg9gr2OKGdwNBDq%2bO5y76ssepk%3d";
-        req.open("GET", url, true); // false means do it synchronously
+        req.open("GET", url, true);
         req.onreadystatechange=function(){
             if (req.readyState==4){
                 if (req.status==200){
                     var eventBox = req.responseText;
                     var time = 0;
-                    var MB = eventBox.slice(eventBox.lastIndexOf(" ", eventBox.indexOf("MB")), eventBox.indexOf("MB"));
+                    var mb = eventBox.slice(eventBox.lastIndexOf(" ", eventBox.indexOf("MB")), eventBox.indexOf("MB"));
 
-                    if (MB.indexOf(",") != -1)
-                        MB = MB.slice(0, MB.indexOf(","));
+                    if (mb.indexOf(",") != -1)
+                        mb = mb.slice(0, mb.indexOf(","));
 
-                    console.log("req: ", MB);
-                    notif("", "Kanguru consumo", MB);
-
-                    if (MB > 60) {
-                        time = MB/4;
-
-                    } else {
-                        console.log("menor");
+                    if (mb > 60)
+                        time = mb/4;
+                    else {
                         setMBdesponiveis();
                         time = 1;
                     }
+                    time *= 60;
 
-                    console.log("TIME: ",time);
-                    var t = setTimeout(function() { getMBdisponiveis() }, time*60*1000)
+                    notif("", "Kanguru consumo", hhmmss(time));
+
+                    console.log("MB: ", mb);
+                    console.log("TIME: ",hhmmss(time));
+                    var t = setTimeout(function() { getMBdisponiveis() }, time*1000)
                 }
             }
         }
@@ -50,7 +59,6 @@ function setMBdesponiveis(){
         }
     }
 
-    console.log("SET OK");
     req.send();
 }
 
